4637926cd4060f8f93d2cb779e6940a7454579d9
edan <molphone0328@gmail.com>
2025-10-31 07:02:57 +0900

    [MAIN] 글꼴 추가, 게임 종료 제작

---
+++ Assets/Scene_Main/Scripts/GameExit.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameExit : MonoBehaviour
+{
+    // 이 함수를 버튼 클릭 이벤트에 연결합니다.
+    public void QuitGame()
+    {
+        // 유니티 에디터에서 실행 중일 경우
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // 빌드된 게임에서 실행 중일 경우
+        Application.Quit();
+#endif
+    }
+}
