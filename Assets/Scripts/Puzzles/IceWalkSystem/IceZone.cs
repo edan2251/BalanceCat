be85608268123404d1eed17038d8dfd5181e2bf0
edan <molphone0328@gmail.com>
2025-12-09 23:28:49 +0900

    [Feat] 3, 4챕터 기본세팅 및 1, 2 챕터 스폰위치 초기화

--- Assets/Scripts/Puzzles/IceWalkSystem/IceZone.cs
+++ Assets/Scripts/Puzzles/IceWalkSystem/IceZone.cs
@@ -3,34 +3,27 @@ using UnityEngine;
 [RequireComponent(typeof(Collider))]
 public class IceZone : MonoBehaviour
 {
-    private void Awake()
+    private void OnCollisionStay(Collision collision)
     {
-        GetComponent<Collider>().isTrigger = true;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            PlayerMovement player = other.GetComponent<PlayerMovement>();
+            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
             if (player != null)
             {
                 player.SetSlippery(true);
-                // [신규] 미끄러질 때도 링이 반응하도록 시뮬레이션 켜기
                 player.SetExternalForceSimulation(true);
             }
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnCollisionExit(Collision collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
-            PlayerMovement player = other.GetComponent<PlayerMovement>();
+            PlayerMovement player = collision.gameObject.GetComponent<PlayerMovement>();
             if (player != null)
             {
                 player.SetSlippery(false);
-                // [신규] 끄기
                 player.SetExternalForceSimulation(false);
             }
         }
